4d90c58236b8fac316d4a89840aa8d0e97573240
TanveerAhmed98 <tanveer.ahm98@gmail.com>
2020-06-18 00:21:25 +0500

    Project Codes

---
+++ JavaScript Assignment - 1/chapter - 14-16/app.js
@@ -0,0 +1,69 @@
+//Ans1
+var myarray1 = [];
+
+//Ans2
+var fruits = new Array( );
+
+//Ans3
+var string_array = ["Tanveer","Jahangeer","Sameer"];
+
+//Ans4
+var numbers_array = [12,23,24,56,78];
+
+//Ans5
+var bool_array = [true,false];
+
+//Ans6
+var mixed_array = ["John", "Doe", 46, true, 47.6];
+
+//Ans7
+var qualifications = ["SSC" , "HSC" , "BSC" , "BS" , "BCOM" , "MS" , "M.PHIL" , "PHD"];
+document.write("<b>Qualifications : </b>");
+document.write("<br><br>");
+document.write("1 ) "+qualifications[0]);
+document.write("<br>");
+document.write("2 ) "+qualifications[1]);
+document.write("<br>");
+document.write("3 ) "+qualifications[2]);
+document.write("<br>");
+document.write("4 ) "+qualifications[3]);
+document.write("<br>");
+document.write("5 ) "+qualifications[4]);
+document.write("<br>");
+document.write("6 ) "+qualifications[5]);
+document.write("<br>");
+document.write("7 ) "+qualifications[6]);
+document.write("<br>");
+document.write("8 ) "+qualifications[7]);
+document.write("<br><br>");
+
+//Ans8
+var student_name = ["Tanveer" , "Sameer" , "Jaweriya"];
+var student_score = [300 , 400 , 450];
+var total_score = 500;
+document.write("Score of "+student_name[0]+" is "+student_score[0] + " & Percentage is: "+student_score[0]/total_score*100)+" .";
+document.write("<br>");
+document.write("Score of "+student_name[1]+" is "+student_score[1] + " & Percentage is: "+student_score[1]/total_score*100)+" .";
+document.write("<br>");
+document.write("Score of "+student_name[2]+" is "+student_score[2] + " & Percentage is: "+student_score[2]/total_score*100)+" .";
+
+//Ans9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
