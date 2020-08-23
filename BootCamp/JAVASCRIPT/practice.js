ed0c1e7ace138ec94ea1830b70038961a3a3113a
TanveerAhmed98 <tanveer.ahm98@gmail.com>
2020-08-23 20:08:13 +0500

    Project Codes

--- BootCamp/JAVASCRIPT/practice.js
+++ BootCamp/JAVASCRIPT/practice.js
@@ -1,1 +1,28 @@
-// JAVASCRIPT PRACTICE CODES
+// First JavaScript Code
+// console.log("Hello World");
+
+// Alert
+// alert("Welcome Bro");
+
+// Console.Log
+// console.log("Tanveer Ahmed");
+
+// Document.Write
+// document.write("Answer is: "+(2+8));
+
+// Variables
+// var name = "Tanveer Ahmed";
+// console.log("My name is: "+name);
+
+// data types
+// var a = "Pakistan";
+// var b = 22;
+// var c = true;
+// var d;
+// var e = null;
+// console.log(a,b,c,d,e);
+
+// Typeof
+// var a = "Tanveer";
+// console.log(typeof(a));
+
