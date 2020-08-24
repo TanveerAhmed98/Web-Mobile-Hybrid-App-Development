331e5f1f4c0fb2bf2325427312fe278a484fdd96
TanveerAhmed98 <tanveer.ahm98@gmail.com>
2020-08-24 23:43:01 +0500

    Project Codes

--- BootCamp/JAVASCRIPT/practice.js
+++ BootCamp/JAVASCRIPT/practice.js
@@ -26,3 +26,71 @@
 // var a = "Tanveer";
 // console.log(typeof(a));
 
+// Comment
+// This is the way to write comment
+
+// Arithmetic Operators
+// var a = 5;
+// var b = 3;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+// Assignment Operators
+// var a = 5;
+// console.log(a +=5);
+// console.log(a -=5);
+// console.log(a *=5);
+// console.log(a /=5);
+// console.log(a %=5);
+// console.log(a **=5);
+
+// Increment & Decrement
+// var a = 5;
+// a++;
+// console.log(a);
+// a--;
+// console.log(a);
+
+// String Concatenation
+// var a = "Tanveer";
+// var b = "Ahmed";
+// var c = a+" "+b;
+// console.log("My name is: "+c);
+
+// Prompt
+// var question = "What is your name";
+// var name = prompt(question);
+// console.log("Your name is: "+name);
+
+// String to Number
+// var gas_bill = prompt("Enter Your Gas Bill");
+// var electricity_bill = prompt("Enter Your Electricity Bill");
+// var total_bill = Number(gas_bill)+Number(electricity_bill);
+// console.log("Your Percentage is: "+total_bill);
+
+// Comparison Operators
+// var a=3;
+// console.log(a==6);
+// console.log(a===6);
+// console.log(a!=6);
+// console.log(a!==6);
+// console.log(a>6);
+// console.log(a<6);
+// console.log(a>=6);
+// console.log(a<=6);
+
+// Logical Operators
+// var a = 50;
+// var b = a>40 && a<60;
+// console.log(b);
+// var b = a>40 || a<45;
+// console.log(b);
+// var b = a>40;
+// console.log(!b);
+
+
+
