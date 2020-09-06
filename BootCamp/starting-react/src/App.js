41678ad2a6d11cf7812afc3745f5cef2c28302ea
TanveerAhmed98 <tanveer.ahm98@gmail.com>
2020-09-06 14:56:30 +0500

    Project Codes

--- BootCamp/starting-react/src/App.js
+++ BootCamp/starting-react/src/App.js
@@ -4,23 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World I am learning React.JS</h1>
+      <img src={require('./1.png')} />
     </div>
   );
 }
-
 export default App;
