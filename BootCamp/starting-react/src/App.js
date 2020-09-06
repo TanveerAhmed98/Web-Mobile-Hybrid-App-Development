4fe805f088ca012f69a841f3f89e09e2c83e4083
TanveerAhmed98 <tanveer.ahm98@gmail.com>
2020-09-06 18:09:48 +0500

    Project Codes

--- BootCamp/starting-react/src/App.js
+++ BootCamp/starting-react/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Hello World I am learning React.JS</h1>
-      <img src={require('./1.png')} />
+      <h1>Hello World Learning React.JS</h1>
+      <img src={require('./1.png')} alt="iamage not found"/>
     </div>
   );
 }
